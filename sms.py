59cd105e691526e0d8c549332fb7f78d54fe6be8
all-drama <90520716+all-drama@users.noreply.github.com>
2022-04-28 17:19:18 +0530

    Update sms.py

--- sms.py
+++ sms.py
@@ -14,33 +14,3 @@ for i in range(100):
         print(resp.status_code)
         
         
-///<?php
-
-$url = "https://api.zepto.co.in/api/v1/user/customer/send-otp-sms/";
-
-$curl = curl_init($url);
-curl_setopt($curl, CURLOPT_URL, $url);
-curl_setopt($curl, CURLOPT_POST, true);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-$headers = array(
-   "Content-Type: application/json",
-);
-curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-
-$data = '{"mobileNumber":""}';
-
-curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-
-//for debug only!
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-
-$resp = curl_exec($curl);
-curl_close($curl);
-var_dump($resp);
-
-?>
-
-///
-
